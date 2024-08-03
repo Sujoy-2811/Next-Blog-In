6192b736b20d1b419032235e8b90f771ef6208fd
Sujoy Manna <sujoy.dev2811@gmail.com>
2024-08-04 01:13:17 +0530

    refactor

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -1,5 +1,5 @@
 import { assets } from "@/assets/assets";
-import Sidebar from "@/Components/AdminComponents/Sidebar";
+import Sidebar from "@/components/adminComponents/Sidebar";
 import Image from "next/image";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 "use client";
 import BlogList from "@/components/BlogList";
-import Footer from "@/Components/Footer";
-import Header from "@/Components/Header";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
